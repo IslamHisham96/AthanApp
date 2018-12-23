2c7365ca94e7d0a7953444c57af58ed0c69bb8d0
IslamHisham96 <islam.hisham96@gmail.com>
2018-12-23 11:00:54 +0200

    added notification

--- app/src/main/java/com/example/islam/project/MyApplication.java
+++ app/src/main/java/com/example/islam/project/MyApplication.java
@@ -1,13 +1,24 @@
 package com.example.islam.project;
 
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.islam.project.Activities.PrayerTimesActicity;
 import com.example.islam.project.Adapters.DBAdapter;
+import com.example.islam.project.Fragments.PrayerTimesFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,12 +40,22 @@ import static com.example.islam.project.Constants.year;
 
 public class MyApplication extends Application {
     private static Context context;
-    public static DBAdapter myDb;
-    public static SharedPreferences sharedPreferences;
+    private static DBAdapter myDb;
+    private static SharedPreferences sharedPreferences;
+    public static String CHANNEL_ID = "com.example.islam.project.channel_id";
+    private static int notificationId = 66;
+    //public static int audioID;
 
     public void onCreate() {
         super.onCreate();
         MyApplication.context = getApplicationContext();
+        /*
+        Log.d(Constants.TAG,"audio"+audioID);
+        audioID = getResources().getIdentifier("athan","raw",getPackageName());
+        Log.d(Constants.TAG,"audio"+audioID);
+        */
+        createNotificationChannel();
+        playNotification("hey","man");
         sharedPreferences = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
         readParams();
         openDB();
@@ -137,5 +158,47 @@ public class MyApplication extends Application {
     }
     public static void setFirst(boolean first){
         sharedPreferences.edit().putBoolean(Constants.FIRST_TIME, first).apply();
+    }
+
+    public static int playNotification(String textTitle, String textContent, Context ctx){
+        Intent intent = new Intent(ctx, PrayerTimesActicity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(ctx, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_menu_calcmethod)
+                .setContentTitle(textTitle)
+                .setContentText(textContent)
+                .setAutoCancel(true)
+                .setContentIntent(PendingIntent.getActivity(ctx, 0, intent, 0))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setVisibility(Notification.VISIBILITY_PUBLIC)
+                .setSound(Uri.parse("android.resource://"
+                        + context.getPackageName() + "/raw/athan"));
+
+        Notification notif = mBuilder.build();
+
+        NotificationManagerCompat.from(ctx)
+                .notify(notificationId, notif);
+
+        return notificationId;
+    }
+    public static int playNotification(String textTitle, String textContent){
+        return playNotification(textTitle,  textContent, getAppContext());
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
